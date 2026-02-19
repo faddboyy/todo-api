ccee932e4c9aa95efdd1247e6d6c90a8dc7694e7
Faddboyy <fadillahmaulanaxyz02@gmail.com>
2026-02-20 03:41:51 +0700

    Main

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
+        'hostname' => getenv('DB_HOST'),
+        'username' => getenv('DB_USERNAME'),
+        'password' => getenv('DB_PASSWORD'),
+        'database' => getenv('DB_DATABASE'),
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
@@ -41,7 +41,7 @@ class Database extends Config
         'compress'     => false,
         'strictOn'     => false,
         'failover'     => [],
-        'port'         => 3306,
+        'port'     => getenv('DB_PORT'),
         'numberNative' => false,
         'foundRows'    => false,
         'dateFormat'   => [
